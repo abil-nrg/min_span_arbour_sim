4c4dadf5eac4d856f580d85ec6f1ee91882e8cde
abil-nur <abil.nurgaliyev@gmail.com>
2024-04-25 13:42:34 -0700

    Worked on step function

--- loop_contraction.py
+++ loop_contraction.py
@@ -6,15 +6,55 @@ from networkx.drawing.nx_pydot import graphviz_layout
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+class HyperEdge:
+    def __init__(self, members):
+        self.members = members #list of vertice labels (ints)
+    def append(self, v):
+        self.members.append(v)
+    def out_edges(self, T):
+        out_edges = []
+        for v in self.members:
+            if(T.nodes[v]["out_degree"] > 0): #a member has a vertex out
+                out_edges = out_edges + T.node[v]["out_edges"]
+        return out_edges # a list of edge values
 
 global T
-T = nx.DiGraph()
+T = nx.MultiDiGraph()
 
 """ Simulate a random walk on the binary tree for N steps. """ 
 def loop_contraction(N, T): 
-    
+    path = [(0,0)]
+    T.add_node(0, out_degree=0, in_edges=[], out_edges=[])
+    ctr = 0
+    cur_step = 0 #start at root
+    for _ in range(N):
+        origin_of_step, next_step, H = step(cur_step, H) #changes the hypergraph
+        
+        if next_step >ctr:#change the multiditree, legality of step is already verified above
+            T.add_node(ctr, out_degree=0, in_edges=[], out_edges=[]) #add the node
+
+            if origin_of_step == cur_step: #no hyper edge was used
+                T.nodes[cur_step]["out_edges"] += 1
+                T.nodes[cur_step]["out_degree"] += 1
+                T.add_edge(cur_step, ctr)
+                path.append((cur_step, next_step))
+            else: #a hyper edge was used
+                T.nodes[origin_of_step]["out_edges"] += 1
+                T.nodes[origin_of_step]["out_degree"] += 1
+                T.add_edge(origin_of_step, ctr)
+                path.append((cur_step, origin_of_step))
+                path.append((origin_of_step, next_step))
     return path  
 
+"""
+Needs a hypergraph, H, and a current vertex, v
+"""
+def step(v, H):
+    #alter the graph
+    #chose randomly
+    origin_of_step = -1
+    next_step = -2
+    return origin_of_step, next_step, H
 #Gives the animation
 def animate_graph_traversal(graph, traversal_order):
     pos = graphviz_layout(T, prog="dot")  # Position nodes using spring layout
